e2d5136f75ac02f3d02b4ada715cd897ba7438a2
doodack <michal.dudak@gmail.com>
2013-11-20 00:28:08 +0100

    Initial commit

---
+++ Web/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System.Web.Mvc;
+
+namespace Web.Controllers
+{
+	public class HomeController : Controller
+	{
+		public ActionResult Index()
+		{
+			return View();
+		}
+
+		public ActionResult Overview()
+		{
+			return View();
+		}
+
+		public ActionResult Api()
+		{
+			return View();
+		}
+
+		public ActionResult Demos()
+		{
+			return View();
+		}
+
+		public ActionResult Download()
+		{
+			return View();
+		}
+	}
+}
---
+++ Web/Scripts/demoapp.js
@@ -0,0 +1,6 @@
+﻿(function (window, ng) {
+	"use strict";
+
+	window.demoApp = ng.module("demoApp", ["light-grid"]);
+
+}(window, window.angular));
