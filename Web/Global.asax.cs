0f4fb85f1b11d5db6cab1ef35e26509ae83e1730
Michał Dudak <michal.dudak@gmail.com>
2015-06-22 15:11:25 +0200

    Error handling

--- Web/Global.asax.cs
+++ Web/Global.asax.cs
@@ -2,6 +2,7 @@
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using Web.Controllers;
 
 namespace Web
 {
@@ -13,6 +14,21 @@ namespace Web
 			FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
 			RouteConfig.RegisterRoutes(RouteTable.Routes);
 			BundleConfig.RegisterBundles(BundleTable.Bundles);
+		}
+
+		protected void Application_EndRequest()
+		{
+			if (Context.Response.StatusCode == 404)
+			{
+				Response.Clear();
+
+				var rd = new RouteData();
+				rd.Values["controller"] = "Error";
+				rd.Values["action"] = "NotFound";
+
+				IController c = new ErrorController();
+				c.Execute(new RequestContext(new HttpContextWrapper(Context), rd));
+			}
 		}
 	}
 }
